104ec7d8e3ea1864c7eae00a5cdb48c7bc93a2fe
Sam Dolby <sam@dolby.org.uk>
2021-11-11 21:20:27 +0000

    sticky nav, animations etc added

---
+++ js/animation.js
@@ -0,0 +1,15 @@
+console.log('hello')
+var header = document.querySelector('.title-header');
+var logos = document.querySelector('.logo-section');
+var text = document.querySelector('.title-mid');
+var button = document.querySelector('.title-bottom');
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    header.setAttribute('id','visible');
+    text.setAttribute('id','visible');
+    logos.setAttribute('id','visible');
+    button.setAttribute('id','visible');
+
+});
